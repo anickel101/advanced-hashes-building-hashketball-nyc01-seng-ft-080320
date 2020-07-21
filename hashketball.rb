e6e84ac71647690393336da9452d1390fd5147a0
Anson Nickel <anson.nickel@gmail.com>
2020-07-21 16:54:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -46,7 +46,7 @@ def game_hash
           :blocks => 8,
           :slam_dunks => 5
         },
-        "Jason Terry" => {
+        { :player_name => "" 
           :number => 31,
           :shoe => 15,
           :points => 19,
