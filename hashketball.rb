4dea4958fb83dca734f7dc6da55fc7e2cc03d9c9
Anson Nickel <anson.nickel@gmail.com>
2020-07-21 16:44:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,8 +6,8 @@ def game_hash
       :team_name => "Brooklyn Nets", 
       :colors => ["Black", "White"],
       binding.pry
-      :players => [
-        {"Alan Anderson" => {
+      :players => [{
+        "Alan Anderson" => {
           :number => 0,
           :shoe => 16,
           :points => 22,
@@ -16,7 +16,7 @@ def game_hash
           :steals => 3,
           :blocks => 1,
           :slam_dunks => 1},
-        {"Reggie Evans" => {
+        "Reggie Evans" => {
           :number => 30,
           :shoe => 14,
           :points => 12,
@@ -24,8 +24,8 @@ def game_hash
           :assists => 12,
           :steals => 12,
           :blocks => 12,
-          :slam_dunks => 7}
-        {"Brook Lopez" => {
+          :slam_dunks => 7},
+        "Brook Lopez" => {
           :number => 11,
           :shoe => 17,
           :points => 17,
@@ -33,8 +33,8 @@ def game_hash
           :assists => 10,
           :steals => 3,
           :blocks => 1,
-          :slam_dunks => 15}
-        {"Mason Plumlee" => {
+          :slam_dunks => 15},
+        "Mason Plumlee" => {
           :number => 1,
           :shoe => 19,
           :points => 26,
@@ -42,8 +42,8 @@ def game_hash
           :assists => 6,
           :steals => 3,
           :blocks => 8,
-          :slam_dunks => 5}
-        {"Jason Terry" => {
+          :slam_dunks => 5},
+        "Jason Terry" => {
           :number => 31,
           :shoe => 15,
           :points => 19,
@@ -52,8 +52,7 @@ def game_hash
           :steals => 4,
           :blocks => 11,
           :slam_dunks => 1}      
-      }
-      ]
+      }]
     },
     :away => {
       :team_name => "Charlotte Hornets", 
