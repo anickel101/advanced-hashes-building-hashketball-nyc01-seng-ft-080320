ca847629a7dfc9985f19d6cc3da21efa44c59a89
Anson Nickel <anson.nickel@gmail.com>
2020-07-21 16:49:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,8 +5,7 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets", 
       :colors => ["Black", "White"],
-      :players => [{
-        "Alan Anderson" => {
+      :players => [:player_name => "A" {
           :number => 0,
           :shoe => 16,
           :points => 22,
